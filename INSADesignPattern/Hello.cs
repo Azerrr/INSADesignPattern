83672f958aa0a5500783455efa463867a7b9a4ba
Azerrr <remi.racault@insa-cvl.fr>
2017-09-26 16:15:45 +0200

    Add observer, and observable classes

---
+++ INSADesignPattern/Hello.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSADesignPattern.Observable
+{
+    class Hello : IObservable
+    {
+        public bool Execute()
+        {
+            Console.WriteLine("Hello, what's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + ", nice to meet you");
+            return true;
+        }
+
+    }
+}
