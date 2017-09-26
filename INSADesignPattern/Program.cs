67a0fbd8612c18ca48d7705cf0267b03d97ec0b5
Azerrr <remi.racault@insa-cvl.fr>
2017-09-26 16:44:50 +0200

    update file

--- INSADesignPattern/Program.cs
+++ INSADesignPattern/Program.cs
@@ -8,11 +8,15 @@ namespace INSADesignPattern
 {
     class Program
     {
+        public static string UserName = "You";
+        
         static void Main(string[] args)
         {
             string line;
-            Console.WriteLine("");
-            Console.WriteLine("     __   __     __  ________  _____");
+            Observable.Obersver obs = new Observable.Obersver();
+
+            Console.WriteLine("                                     ");
+            Console.WriteLine("     __   __     __  ________  _____ ");
             Console.WriteLine("    /  / /  |  /  / /  _____/ /  _  |");
             Console.WriteLine("   /  / /   | /  / /  /____  /  /_| |");
             Console.WriteLine("  /  / /    |/  / /____   / /  ___  |");
@@ -21,10 +25,23 @@ namespace INSADesignPattern
             Console.WriteLine("Desing Patterns - Anthony Maudry amaudry@gmail.com");
             Console.WriteLine("Hello,");
             Console.WriteLine("Write something (type 'exit' to exit the program).");
+
+            Observable.Hello hello = new Observable.Hello();
+            Observable.Smiley smile = new Observable.Smiley();
+            obs.OnEvent("hi", smile);
+            obs.OnEvent("hello", smile);
+            obs.OnEvent("hello", hello);
+            //obs.OnEvent("hi", smile);
+            
+
             while ((line = Console.ReadLine()) != "exit")
             {
-                Console.WriteLine("You wrote : ");
-                Console.WriteLine(line);
+                if (!obs.trigger(line))
+                {
+                    Console.WriteLine(UserName + " wrote : ");
+                    Console.WriteLine(line);
+                }
+
             }
 
             Console.WriteLine("Goodbye.");
